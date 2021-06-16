f0e5247c23637a9e64a3056af2e1d7f6b0b3d5a4
imbesat-rizvi <imbugene@gmail.com>
2021-06-16 12:01:54 +0530

    Initial release v0.1.0 with tested (non-language model based) published wheel file on pypi.

--- setup.py
+++ setup.py
@@ -11,36 +11,46 @@ from pathlib import Path
 
 curr_dir = Path(__file__).parent
 
-with open(curr_dir/"requirements.txt", "r") as f:
+with open(curr_dir / "requirements.txt", "r") as f:
     # include all requirements which are not commented
     reqs = [i for i in f.read().splitlines() if i.strip()]
-    install_requires = reqs[reqs.index("#required")+1:reqs.index("#speechlm")]
-    extras_require = {"speechlm": [x[1:] for x in reqs[reqs.index("#speechlm")+1:]]}
+    install_requires = reqs[reqs.index("#required") + 1 : reqs.index("#speechlm")]
+    extras_require = {"speechlm": [x[1:] for x in reqs[reqs.index("#speechlm") + 1 :]]}
 
 description = r"""Named after a spell in the Harry Potter Universe, where it 
     amplies the sound of a speaker. In muggles' terminology, this is a repository 
     of modules for audio and speech processing for and on top of machine learning 
     based tasks such as speech-to-text."""
 
-README = (curr_dir/"README.md").read_text()
+README = (curr_dir / "README.md").read_text()
 
-setup(name="sonorus",
+setup(
+    name="sonorus",
     version="0.1.0",
     description=description,
     long_description=README,
+    long_description_content_type="text/markdown",
     author="Md Imbesat Hassan Rizvi",
     author_email="imbugene@gmail.com",
     url="https://github.com/pensieves/sonorus",
     download_url="https://github.com/pensieves/sonorus/releases",
-    packages=find_packages(exclude=["contrib", "docs", "examples", "tests"]), # Required
+    packages=find_packages(
+        exclude=["contrib", "docs", "tests"]
+    ),  # Required
     install_requires=install_requires,
     extras_require=extras_require,
     license="MIT",
-    keywords=["deep learning", "speech recognition", "speech to text", "language modelling"],
+    keywords=[
+        "deep learning",
+        "speech recognition",
+        "speech to text",
+        "language modelling",
+    ],
     classifiers=[
+        "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Langauge :: Python :: 3",
+        "Environment :: GPU :: NVIDIA CUDA :: 11.2",
     ],
 )
